befc00338abedf60da580adf3702d09fda1a64b7
Marco <marco_carrillo@yahoo.com>
2019-11-13 23:44:05 -0500

    Wednesday night upload

---
+++ assets/js/format_screen.js
@@ -0,0 +1,36 @@
+//*********************************************************************/
+// This function will change some attributes based on screen width.   */
+// We don't use screen height to evaluate collapsed status.           */
+// For reference of the developer, the following breaking points      */
+// are followed.                                                      */
+// xlg - > 1200 pixels                                                */
+// lg - 992 to 1199 pixels                                            */
+// md - 768 to 991 pixels                                             */
+// sm - 576 to 767 pixels                                             */
+// xsm - less than 576 pixels                                         */
+//*********************************************************************/
+
+//get screen width.  Togglepoint at screens smaller thatn 992 pixels
+
+var width = $(window).width();
+console.log("First evaluation when it runs, width:  "+width) 
+
+//*********************************************************************/
+// Originally created with menu borders, we need to take them out in case
+// the browse is open originally with a device with less than 992 pixesl
+//*********************************************************************/
+
+if (width<992) $("#menuoption1,#menuoption2").attr("class","nav-link");
+
+//*********************************************************************/
+// Refresh on resizing of the screen
+//*********************************************************************/
+
+$(window).resize(function() {
+    var width = $(window).width();
+    console.log("This is a resize event, width:  "+width) 
+    
+    if   (width<992) $("#menuoption1,#menuoption2").attr("class","nav-link");
+    else $("#menuoption1,#menuoption2").attr("class","nav-link border-right")
+    
+});
